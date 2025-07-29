82043f80324a3b603c0026e6aa6ea3226195264c
Felipe Fernando De Jesus Silva <felipe337fernando@gmail.com>
2025-07-29 13:38:57 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
+class FormSumbit {
+  constructor(settings){
+    this.settings = settings;
+    this.form = document.queryselector(settings.form);
+    this.formButton = document.queryselector(settings.button);
+    if(this.form) {
+      this.url = this.form.getAttribute("action");
+      
+    }
+  }
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+ DisplayError() {
+    this.form.innerHTML = this.settings.error;
+ }
+  init() {
+    if (this.form) this.formbutton.addEventlistener("click", () => this.displaySuccess());
+    return this;
+  }
+}
 
+const formSumbit = new FormSumbit();
